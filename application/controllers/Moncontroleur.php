95e77b0918fa6bf680641c1d8762873b3524bab8
bleuwers <bastienleuwers2@gmail.com>
2021-02-23 15:12:40 +0100

    config bdd

---
+++ application/controllers/Moncontroleur.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Moncontroleur extends CI_Controller {
+	public function index()
+	{
+		echo 'Hello world !';
+	}
+    public function mafonction() {
+		echo 'Je fais ici ce qui me plaît !';
+	} 
+}
+/* End of file moncontroleur.php */
+/* Location: ./application/controllers/Moncontroleur.php */
