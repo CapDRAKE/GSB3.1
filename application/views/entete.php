a07f1c685c1fbd69181750f88ee3f82f1c3ebc36
Sethaxe <thomas77350@orange.fr>
2021-03-08 08:43:09 +0100

    dza

--- application/views/entete.php
+++ application/views/entete.php
@@ -3,8 +3,12 @@
 		<title>Gestion des conférences</title>
 		<?php $this->load->helper('html');
 		echo meta('Content-type', 'text/html; charset=UTF-8', 'equiv');
-		echo  link_tag('css/cssGeneral.css'); ?>
+		echo  link_tag('../css/cssHeader.css'); ?>
 	</head>
 <body>
-  <h1>Gestion des conférences</h1>
-  <div id="contact">
+	<div class = "headerStyle">
+
+		<h1>Gestion des conférences</h1>
+	
+	</div>
+  		<div id="contact">
